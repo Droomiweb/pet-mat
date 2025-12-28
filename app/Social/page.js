f7e043c71da55736005934e7b61ffeb15af2ac71
droomiweb <designer.abinabi@gmail.com>
2025-12-28 13:28:11 +0530

    various updates2

---
+++ app/Social/page.js
@@ -0,0 +1,8 @@
+export default function SocialPage() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl font-bold">Social Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
